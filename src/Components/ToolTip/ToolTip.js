2e2d387a50f3b0f2a2b282c3478708ea55171a18
anishvimal1 <117886030+anishvimal@users.noreply.github.com>
2024-08-08 11:50:33 +0530

    ToolTip & ToolTipCard components updated

--- src/Components/ToolTip/ToolTip.js
+++ src/Components/ToolTip/ToolTip.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import "./ToolTip.css";
-
 import Center from "../Center/Center";
 import ToolTipCard from "../ToolTipCard/ToolTipCard";
 
@@ -9,16 +7,34 @@ const ToolTip = () => {
     return (
         <Center>
             <div className="tooltip_wrap">
-                <ToolTipCard date='2024-07-16' text='Running ' color square value='380' />
-                <ToolTipCard date='2024-07-16' text='Running ' color square value='380' />
-                <ToolTipCard date='2024-07-16' text='Running ' color square value='380' />
-                <ToolTipCard text='Running ' value='380' />
-                <ToolTipCard title='Activities' text='Running ' color square value='380' />
-                <ToolTipCard title='Activities' text='Running ' totalValue="330" total color square value='380' />
-                <ToolTipCard title='Activities' text='Running ' totalValue="330" total extension='KCl' color square value='380' />
+                <ToolTipCard
+                    date="2024-07-16"
+                    bodyItems={[
+                        { color: 'red', square: true, text: 'Running', value: '380' },
+                        { color: 'blue', square: true, text: 'Cycling', value: '120' }
+                    ]}
+                />
+                <ToolTipCard
+                    title="Activities"
+                    bodyItems={[
+                        { color: 'green', square: true, text: 'Swimming', value: '250' },
+                        { color: 'yellow', square: true, text: 'Hiking', value: '450' }
+                    ]}
+                    totalValue="700"
+                    total
+                />
+                <ToolTipCard
+                    title="Activities"
+                    bodyItems={[
+                        { color: 'orange', square: true, text: 'Yoga', value: '150' }
+                    ]}
+                    totalValue="150"
+                    total
+                    extension="KCal"
+                />
             </div>
         </Center>
-    )
-}
+    );
+};
 
 export default ToolTip;
--- src/Components/ToolTipCard/ToolTipCard.js
+++ src/Components/ToolTipCard/ToolTipCard.js
@@ -1,46 +1,45 @@
 import React from "react";
-
 import './ToolTipCard.css';
 
-const ToolTipCard = ({ date, color, icon, text, value, total, totalValue, square, title, extension }) => {
+const ToolTipCard = ({ date, title, bodyItems, total, totalValue, extension }) => {
     return (
         <div className="tooltip_card_wrap">
             <div className="tooltip_card">
                 <div className="content">
-                    {date && (
-                        <span className="date">{date}</span>
-                    )}
-                    {title && (
-                        <span className="date">{title}</span>
-                    )}
-                    <div className="body">
-                        <div>
-                            {color && (
-                                <span className={`${color} ${square ? 'square' : ''}`}>{color}</span>
-                            )}
-                            {icon && (
-                                <span className={`${icon}`}></span>
-                            )}
-                            <span className="text">{text}</span>
+                    {date && <span className="date">{date}</span>}
+                    {title && <span className="title">{title}</span>}
+
+                    {/* Map through the bodyItems array to create multiple body sections */}
+                    {bodyItems.map((item, index) => (
+                        <div className="body" key={index}>
+                            <div>
+                                {item.color && (
+                                    <span className={`${item.color} ${item.square ? 'square' : ''}`}></span>
+                                )}
+                                {item.icon && (
+                                    <span className={`${item.icon}`}></span>
+                                )}
+                                <span className="text">{item.text}</span>
+                            </div>
+                            <div>
+                                <span className="value">{item.value}</span>
+                            </div>
                         </div>
-                        <div>
-                            <span className="value">{value}</span>
-                        </div>
-                    </div>
+                    ))}
+
                     {total && totalValue && (
                         <div className="totalValue">
                             <span className="total">Total</span>
-                            <span className="total_value">{totalValue}
-                                {extension && (
-                                    <span>{extension}</span>
-                                )}
+                            <span className="total_value">
+                                {totalValue}
+                                {extension && <span>{extension}</span>}
                             </span>
                         </div>
                     )}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ToolTipCard;
